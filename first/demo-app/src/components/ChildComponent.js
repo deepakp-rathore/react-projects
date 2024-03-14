ca423e2c3a9b0e1228d523c7ba3c521577660f23
Deepak <deepak.yourdomain@gmail.com>
2024-03-14 14:31:04 +0530

    Initial commit

--- first/demo-app/src/components/ChildComponent.js
+++
@@ -1,9 +0,0 @@
-export const ChildComponent = (props) => {
-    return (
-        <div>
-            <button onClick={() => props.greetHandler('child')}>
-                Greet
-            </button>
-        </div>
-    )
-}
